b429b36ab8ae1b95cfe7087e38045511d106f968
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-03 14:45:22 -0500

    prepared server for first time user

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { db } = require("../models/transaction.js");
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
@@ -24,6 +25,16 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
+      if (dbTransaction.length === 0) {
+        let firstTime = {
+          "_id": "615a056023b89239ebf49a02",
+          "name": "transaction 4",
+          "value": 6,
+          "date": "2021-10-03T19:32:48.933Z",
+          "__v": 0
+      }
+        res.json([firstTime])
+      }
       res.json(dbTransaction);
     })
     .catch(err => {
